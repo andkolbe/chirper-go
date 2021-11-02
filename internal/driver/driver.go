c6fd40b3c266a52d075972695e00137939e04d09
Andrew <kolbe1129@gmail.com>
2021-11-02 12:30:58 -0500

    added comments to users model

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -5,11 +5,18 @@ import (
 	"log"
 	"time"
 
+	// We don't use anything in the mysql package directly, which means that the Go compiler will raise an error if we try to import it normally
+	// But we need the mysql package's init() function to run so that our driver can register itself with database/sql
+	// We get around this by aliasing the package name to the blank identifier
+	// This means mysql.init() still gets executed, but the alias is harmlessly discarded (and our code runs error-free)
+	// This approach is standard for most of Go's SQL drivers
 	_ "github.com/go-sql-driver/mysql"
 )
 
 // creates a new database for the application
 func DBConnect(dsn string) (*sql.DB, error) {
+	// initialise a new sql.DB object by calling sql.Open()
+	// the sql.DB object it returns is not a database connection – it's an abstraction representing a pool of underlying connections
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err	
@@ -24,8 +31,16 @@ func DBConnect(dsn string) (*sql.DB, error) {
 
 	log.Println("Pinged db!")
 
+	// Set the maximum number of concurrently open connections (in-use + idle) to 10. 
+	// Setting this to less than or equal to 0 will mean there is no maximum limit (which is also the default setting)
 	db.SetMaxOpenConns(10)
-	db.SetConnMaxIdleTime(5)
+
+	// Set the maximum number of concurrently idle connections to 5. Setting this to less than or equal to 0 will mean that no idle connections are retained
+	db.SetMaxIdleConns(5)
+
+
+	// Set the maximum lifetime of a connection to 5 minutes
+	// Setting it to 0 means that there is no maximum lifetime and the connection is reused forever (which is the default behavior)
 	db.SetConnMaxLifetime(5 * time.Minute)
 
 	return db, nil
--- internal/models/users.go
+++ internal/models/users.go
@@ -8,6 +8,7 @@ import (
 )
 
 // we can only use string and int safely because we set NOT NULL constraints on all of the columns on the table
+// If the table contained nullable fields we would need to use the sql.NullString and sql.NullInt types instead
 type User struct {
 	ID         string    `json:"id"`
 	Name       string    `json:"name"`
@@ -22,24 +23,38 @@ type UserModel struct {
 }
 
 // Use a method on the custom UserModel type to run the SQL query.
+// GET All Users
 func (m UserModel) GetAllUsers() ([]User, error) {
+	// fetch a result set from the userss table using the DB.Query() method and assign it to a rows variable
 	rows, err := m.DB.Query("SELECT * FROM users")
 	if err != nil {
 		log.Println(err)
 		return nil, err
 	}
+	// Closing a result set properly is important
+	// As long as a resultset is open it will keep the underlying database connection open – which in turn means the connection is not available to the pool
+	// So if something goes wrong and the result set isn't closed it can rapidly lead to all the connections in your pool being used up
+	// the defer statement should come after you check for an error from DB.Query
+	// Otherwise, if DB.Query() returns an error, you'll get a panic trying to close a nil resultset
 	defer rows.Close()
 
 	var users []User
+	// use rows.Next() to iterate through the rows in the result set
+	//  This preps the first (and then each subsequent) row to be acted on by the rows.Scan() method
+	// if iteration over all of the rows completes then the result set automatically closes itself and frees-up the connection
 	for rows.Next() {
 		var user User
+		// We use the rows.Scan() method to copy the values from each field in the row to a new User object that we created
 		err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.Created_At)
 		if err != nil {
 			log.Println(err)
 			return nil, err
 		}
+		// add the new User to the users slice we created earlier
 		users = append(users, user)
 	}
+	// When our rows.Next() loop has finished we call rows.Err(). This returns any error that was encountered during the interation
+	// It's important to call this – don't just assume that we completed a successful iteration over the whole resultset
 	if err = rows.Err(); err != nil {
 		log.Println(err)
 		return nil, err
@@ -48,8 +63,17 @@ func (m UserModel) GetAllUsers() ([]User, error) {
 	return users, nil
 }
 
+// GET One User
 func (m UserModel) GetUserByID(id string) (User, error) {
 
+	// Because we need to include untrusted input (the id variable) in our SQL query, we take advantage of placeholder parameters, passing in the value of our 
+	// placeholder as the second argument to DB.QueryRow()
+	// Behind the scenes, db.QueryRow (and also db.Query() and db.Exec()) work by creating a new prepared statement on the database, 
+	// and subsequently execute that prepared statement using the placeholder parameters provided
+	// This means that all three methods are safe from SQL injection when used correctly
+	// Prepared statements are resilient against SQL injection, because parameter values, which are transmitted later using a different protocol, 
+	// need not be correctly escaped
+	// If the original statement template is not derived from external input, injection cannot occur
 	row := m.DB.QueryRow("SELECT * FROM users WHERE id = ?", id)
 
 	var user User
@@ -61,15 +85,19 @@ func (m UserModel) GetUserByID(id string) (User, error) {
 	return user, nil
 }
 
+// POST
 func (m UserModel) CreateNewUser(user User) int64 {
 
 	// add password hashing
 
+	// DB.Exec(), like DB.Query() and DB.QueryRow(), is a variadic function, which means you can pass in as many parameters as you need
+	// The db.Exec() method returns an object satisfying the sql.Result interface, which you can either use or discard with the blank identifier
 	res, err := m.DB.Exec("INSERT INTO users (name, email, password) VALUES(?, ?, ?)", &user.Name, &user.Email, &user.Password)
 	if err != nil {
 		log.Fatalf("Unable to execute the query. %v", err)
 	}
 
+	// The sql.Result() interface guarantees two methods: LastInsertId() – which is often used to return the value of an new auto increment id
 	id, err := res.LastInsertId()
 	if err != nil {
 		return 0
@@ -80,12 +108,14 @@ func (m UserModel) CreateNewUser(user User) int64 {
 	return id
 }
 
+// PUT
 func (m UserModel) UpdateUser(user User, id string) int64 {
 	res, err := m.DB.Exec("UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?", &user.Name, &user.Email, &user.Password, id)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// The sql.Result() interface guarantees two methods: RowsAffected() – which contains the number of rows that the statement affected
 	rowsAffected, err := res.RowsAffected()
 	if err != nil {
         log.Fatalf("Error while checking the affected rows. %v", err)
@@ -95,6 +125,7 @@ func (m UserModel) UpdateUser(user User, id string) int64 {
 	return rowsAffected
 }
 
+// DELETE
 func (m UserModel) DeleteUser(id string) int64{
 	res, err := m.DB.Exec("DELETE FROM users WHERE id = ?", id)
 	if err != nil {
